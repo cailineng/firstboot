468688837a792d02679fe52daf535274782e7682
cailineng <329956247@qq.com>
2017-12-02 20:57:22 +0800

    test2057

--- src/main/java/FirstController.java
+++ src/main/java/FirstController.java
@@ -6,7 +6,7 @@ public class FirstController {
         System.out.println("haha");
     }
     public void tes3t(){
-        System.out.println("haha");
+        System.out.println("hahafefe");
     }
 
     public void tes3t3(){
