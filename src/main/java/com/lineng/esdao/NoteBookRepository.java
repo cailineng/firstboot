9ada45830f1c2e9ccd49e796e9e635060a9d5d38
c_neng <Cailineng1>
2019-06-18 18:35:21 +0800

    没有es的版本

--- src/main/java/com/lineng/esdao/NoteBookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lineng.esdao;
-
-import com.lineng.esmodel.NoteBook;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface NoteBookRepository extends ElasticsearchRepository<NoteBook,String> {
-
-}
